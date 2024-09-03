63b1795410694f5af630fb3fd051d27fbed52031
Germo Aabla <germo.aabla.it@gmail.com>
2024-09-03 19:44:25 +0300

    update 1

--- Week1FirstApp/Program.cs
+++ Week1FirstApp/Program.cs
@@ -1,7 +1,13 @@
 ﻿// comment
 
+
 string myName = "Germo";
 string myLName = "Aabla";
 
+string name = $"{myName} {myLName}";
+
+
 //Console.WriteLine("Hello " + myName);
-Console.WriteLine($"Hello, {myName} {myLName}!");
+
+//Console.WriteLine($"Hello, {myName} {myLName}!");
+Console.WriteLine($"Hello, {name}!");
--- Week1FirstApp/obj/Debug/net8.0/Week1FirstApp.AssemblyInfo.cs
+++ Week1FirstApp/obj/Debug/net8.0/Week1FirstApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Week1FirstApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25ad130e3254bf7490394eb6fb134123e61ac6ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("Week1FirstApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Week1FirstApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
